19958e8b6b5ef12b68b3b6fe263ede4de56cab39
User <u96464224@gmail.com>
2024-08-09 23:24:46 +0900

    modify

--- dist/index.js
+++ dist/index.js
@@ -3,22 +3,35 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.TicTacToe = void 0;
 const promises_1 = __importDefault(require("readline/promises"));
 const rlp = promises_1.default.createInterface({
     input: process.stdin,
-    output: process.stdout,
 });
+const CLI = {
+    async input() {
+        let input = NaN;
+        while (Number.isNaN(input) || input < 0 || input > 8) {
+            console.log("index : 0~8");
+            input = parseInt(await rlp.question("index 0~8: "));
+        }
+        return input;
+    }
+};
 class TicTacToe {
+    board;
+    turn = "O";
     constructor() {
         this.board = this.boardInit();
+        this.turn = "O";
     }
     boardInit() {
         return new Array(9).fill(" ");
     }
     display() {
-        for (let i = 0; i < 3; i++) {
+        for (let i = 0; i < 9; i += 3) {
             console.log(`${this.board[i]}|${this.board[i + 1]}|${this.board[i + 2]}`);
-            if (i < 2) {
+            if (i < 6) {
                 console.log(`- - -`);
             }
         }
@@ -44,7 +57,7 @@ class TicTacToe {
         return false;
     }
     checkRow() {
-        for (let i = 0; i < 3; i += 3) {
+        for (let i = 0; i < 9; i += 3) {
             if (this.board[i] === this.board[i + 1] && this.board[i] === this.board[i + 2] && this.board[i] !== ' ') {
                 return true;
             }
@@ -55,12 +68,43 @@ class TicTacToe {
         if (this.board[0] === this.board[4] && this.board[0] === this.board[8] && this.board[0] !== ' ') {
             return true;
         }
-        if (this.board[2] === this.board[4] && this.board[0] === this.board[6] && this.board[0] !== ' ') {
+        if (this.board[2] === this.board[4] && this.board[2] === this.board[6] && this.board[2] !== ' ') {
             return true;
         }
         return false;
     }
+    async makeMove() {
+        const num = await CLI.input();
+        this.board[num] = this.turn;
+        this.updateTurn();
+    }
+    updateTurn() {
+        this.turn = this.turn === "O" ? "X" : "O";
+    }
 }
-const ticTacToe = new TicTacToe();
-ticTacToe.display();
-rlp.close();
+exports.TicTacToe = TicTacToe;
+class Game {
+    tictactoe;
+    isGameOver;
+    constructor() {
+        this.tictactoe = new TicTacToe();
+        this.isGameOver = false;
+    }
+    async start() {
+        console.log("game start");
+        this.tictactoe.display();
+        console.log("");
+        while (!this.isGameOver) {
+            await this.tictactoe.makeMove();
+            if (this.tictactoe.hasWinningLine()) {
+                this.isGameOver = true;
+                this.tictactoe.display();
+            }
+            console.clear();
+            this.tictactoe.display();
+        }
+        rlp.close();
+    }
+}
+const game = new Game;
+game.start();
--- src/index.ts
+++ src/index.ts
@@ -2,37 +2,39 @@ import readlinePromises from 'readline/promises';
 
 const rlp = readlinePromises.createInterface({
     input: process.stdin,
-    output: process.stdout,
 });
 
 
-//const userInput = async() => {
-//    try{
-//        const input = await rlp.question("入力: ");
-//        console.log(`入力された内容: ${input}`);
-//    } catch (error){
-//        console.error(error);
-//    } finally {
-//        rlp.close();
-//    }
-//}
+const CLI = {
+    async input(){
+        let input :number = NaN;
+        while(Number.isNaN(input) ||input<0||input>8){
+            console.log("index : 0~8");
+            input = parseInt(await  rlp.question("index 0~8: "));
+        }
+        return input;
+    }
+}
 
 type boardState = " " | "O" | "X";
+type TurnState = "O" | "X";
 
-class TicTacToe {
+export class TicTacToe {
     board: boardState[];
+    turn: TurnState = "O";
     constructor(){
         this.board = this.boardInit();
+        this.turn = "O";
     }
 
-    boardInit(): boardState[]{
+    private boardInit(): boardState[]{
         return new Array(9).fill(" ");
     }
 
     display(){
-        for(let i=0;i<3;i++){
+        for(let i=0;i<9;i+=3){
             console.log(`${this.board[i]}|${this.board[i+1]}|${this.board[i+2]}`);
-            if(i<2){
+            if(i<6){
                 console.log(`- - -`)
             }
         }
@@ -54,7 +56,7 @@ class TicTacToe {
         return false;
     }
 
-    checkColumn(): boolean {
+    private checkColumn(): boolean {
         for(let i=0;i<3;i++){
             if(this.board[i] === this.board[i+3] && this.board[i] === this.board[i+6] && this.board[i]!==' '){
                 return true;
@@ -63,8 +65,8 @@ class TicTacToe {
         return false;
     }
 
-    checkRow(): boolean {
-       for(let i=0;i<3;i+=3){
+    private checkRow(): boolean {
+       for(let i=0;i<9;i+=3){
             if(this.board[i]===this.board[i+1] && this.board[i]===this.board[i+2] && this.board[i]!==' '){
                 return true;
             }
@@ -72,19 +74,58 @@ class TicTacToe {
         return false;
     }
 
-    checkDiagonal(): boolean {
+    private checkDiagonal(): boolean {
         if(this.board[0]===this.board[4] && this.board[0]===this.board[8] && this.board[0]!==' '){
             return true;
         }
 
-        if(this.board[2]===this.board[4] && this.board[0]===this.board[6] && this.board[0]!==' '){
+        if(this.board[2]===this.board[4] && this.board[2]===this.board[6] && this.board[2]!==' '){
             return true;
         }
         return false;
     }
     
+    async makeMove(){
+        const num = await CLI.input();
+        this.board[num] = this.turn;
+        this.updateTurn();
+    }
+
+    private updateTurn(){
+        this.turn = this.turn === "O" ? "X" : "O";
+    }
+
 }
 
-const ticTacToe = new TicTacToe();
-ticTacToe.display();
-rlp.close();
+
+class Game {
+    private tictactoe: TicTacToe;
+    private isGameOver: boolean;
+
+    constructor() {
+        this.tictactoe = new TicTacToe();
+        this.isGameOver = false;
+    }
+
+    async start() {
+        console.log("game start");
+        this.tictactoe.display();
+        console.log("");
+
+        while (!this.isGameOver) {
+            await this.tictactoe.makeMove();
+            if(this.tictactoe.hasWinningLine()){
+                this.isGameOver = true;
+                this.tictactoe.display();
+            }
+            console.clear();
+            this.tictactoe.display();
+        }
+
+        rlp.close();
+    }
+}
+
+
+const game = new Game;
+game.start();
